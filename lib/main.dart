96c37b7b1374e58c33dacf2743d48bb607db2a78
Dries <dries.dubois@gmail.com>
2023-03-21 11:23:17 +0100

    oefening5

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/detail.dart';
+
 // import 'package:flutter_application_2/models/enums.dart';
 import 'models/data.dart';
 
@@ -21,8 +23,14 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
-  Map<String, String> lijstMap = { 'all': "all", 'Resistance': 'Resistance', 'Sith': "Sith",'Jedi Order':'Jedi Order' };
-  late String affiliation ="all";
+  Map<String, String> lijstMap = {
+    'all': "all",
+    'Resistance': 'Resistance',
+    'Sith': "Sith",
+    'Jedi Order': 'Jedi Order'
+  };
+  late String affiliation = "all";
+
   @override
   Widget build(BuildContext context) {
     List<DropdownMenuItem<String>> opties = <DropdownMenuItem<String>>[];
@@ -31,17 +39,30 @@ class _MainState extends State<Main> {
     });
 
     return Scaffold(
-      appBar: AppBar(title:Center(child: Text("Star Wars Characters"),)),
+      appBar: AppBar(
+          title: Center(
+        child: Text("Star Wars Characters"),
+      )),
       body: Container(
-        child: Column(children: [Text("Select a side:"),
-          Center(
-            child: DropdownButton(items: opties,value:affiliation, onChanged: (value){
-              setState(() {
-                affiliation = value!;
-              });
-            }),
-          )
-        ,Text("There are ${characters.length} characters!"),Container(height:500,child:CharacterList(affiliation))],),
+        child: Column(
+          children: [
+            Text("Select a side:"),
+            Center(
+              child: DropdownButton(
+                  items: opties,
+                  value: affiliation,
+                  onChanged: (value) {
+                    setState(() {
+                      affiliation = value!;
+                    });
+                  }),
+            ),
+            Text("There are ${characters.length} characters!"),
+            Expanded(
+                child:
+                    Container(height: 500, child: CharacterList(affiliation)))
+          ],
+        ),
       ),
     );
   }
@@ -49,35 +70,48 @@ class _MainState extends State<Main> {
 
 class CharacterList extends StatefulWidget {
   final String affiliation;
-  const CharacterList (this.affiliation);
+
+  const CharacterList(this.affiliation);
 
   @override
   State createState() => _CharacterListState();
 }
 
 class _CharacterListState extends State<CharacterList> {
-
   @override
   Widget build(BuildContext context) => ListView.builder(
-    itemCount: characters.length,
-    itemBuilder: (BuildContext context, int index) {
-      if (widget.affiliation=="all"){
-      return ListTile(
-        // leading: Image.asset(characters[index].imageAsset),
-        title: Text(characters[index]["name"]),
+        itemCount: characters.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (widget.affiliation == "all") {
+            return ListTile(
+              // leading: Image.asset(characters[index].imageAsset),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          DetailPage(characters[index]["name"])),
+                );
+              },
+              title: Text(characters[index]["name"]),
+            );
+          } else {
+            if (characters[index]["affiliations"]
+                .contains(widget.affiliation)) {
+              return ListTile(
+                // leading: Image.asset(characters[index].imageAsset),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            DetailPage(characters[index]["name"])),
+                  );
+                },
+                title: Text(characters[index]["name"]),
+              );
+            }
+          }
+        },
       );
-    } else {
-        if (characters[index]["affiliations"].contains(widget.affiliation)){
-        return ListTile(
-          // leading: Image.asset(characters[index].imageAsset),
-          title: Text(characters[index]["name"]),
-        );}
-      }
-  },
-  );
-
-
-
 }
-
-
