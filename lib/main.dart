50b4de278ff91e5e1078083c3b030dcefe0217f0
Dries <dries.dubois@gmail.com>
2023-03-21 09:38:43 +0100

    oefening1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/data.dart';
 
 void main() {
   runApp(MainApp());
@@ -11,14 +12,35 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
   const Main({super.key});
 
   @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  Map<String, String> lijstMap = { 'all': "all", 'resistance': 'resistance', 'sith': "sith",'jediOrder':'jediOrder' };
+  late String affiliation ="all";
+  @override
   Widget build(BuildContext context) {
+    List<DropdownMenuItem<String>> opties = <DropdownMenuItem<String>>[];
+    lijstMap.keys.forEach((element) {
+      opties.add(DropdownMenuItem(child: Text(element), value: element));
+    });
+
     return Scaffold(
-      body: Center(
-        child: Text('In a galaxy far far away...'),
+      appBar: AppBar(title:Center(child: Text("Star Wars Characters"),)),
+      body: Container(
+        child: Column(children: [Text("Select a side:"),
+          Center(
+            child: DropdownButton(items: opties,value:affiliation, onChanged: (value){
+              setState(() {
+                affiliation = value!;
+              });
+            }),
+          )
+        ,Text("There are ${characters.length} characters!")],),
       ),
     );
   }
